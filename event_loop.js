238736f2b75875ad3b8eaaecc057130d04dc9528
ElBugs99 <jpvaldivia99@gmail.com>
2023-10-28 20:47:26 -0300

    .

--- event_loop.js
+++ event_loop.js
@@ -4,7 +4,7 @@ http.createServer((req, res) => {
     if ( req.url === '/') {
         res.write('hello world')
 
-        //blocking code
+        //blocking code affects runtime, must be asyncronous
         for ( let i = 0; i < 100000; i++) {
             console.log(Math.random() * i)
         }
