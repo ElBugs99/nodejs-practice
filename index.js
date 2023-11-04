6810f34172503804e669bdda1d765b39920c54ce
ElBugs99 <jpvaldivia99@gmail.com>
2023-11-04 04:47:54 -0300

    added post users

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const express = require('express'); //frameWork nodejs crea servidores
+const express = require('express'); //frameWork
 
-//express(); al ejecutarse crea un servidor
+//express(); creates a server on execution
 
 const app = express();
+
+//make express understand data structures
+app.use(express.text())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile('./static/index.html', {
         root: __dirname
+    })
+})
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('usuario creado')
+})
+
+app.get('/api', (req, res) => {
+    res.json({
+        "user": "JP",
+        age: 56,
+        autism: true
     })
 })
 
@@ -19,5 +36,5 @@ app.post('/products', (req, res) => {
     res.send('creando')
 })
 
-            //puerto
+            //port
 app.listen(3000, () => console.log('server on port 3000'))
