037a1b29d7f1e4475dddd6fd3cda62200a9dd763
ElBugs99 <jpvaldivia99@gmail.com>
2023-11-07 23:51:32 -0300

    started basic server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //REST API CRUD
+
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.listen(3000, )
+
+console.log('server on port 3000')
